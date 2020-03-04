0f1627a1381bc267f663a3c99b4f51575f16aaa8
Benjamin Nwokeleme <benwokeleme@gmail.com>
2020-03-04 18:52:36 -0500

    update mdnSample 03042020

--- lab_4/main.js
+++ lab_4/main.js
@@ -1,2 +1,38 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+//const myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'firefox.jfif') {
+      myImage.setAttribute ('src','Firefox-logo.webp');
+    } else {
+      myImage.setAttribute ('src','firefox.jfif');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
