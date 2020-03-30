8ec059211f8faba7d2a1c966fbbd2adf4dda3219
Benjamin Nwokeleme <benwokeleme@gmail.com>
2020-03-30 05:58:53 -0400

    commit

--- Assignment_2/main.js
+++ Assignment_2/main.js
@@ -5,9 +5,36 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+let lineBreak = document.createElement('br');
+thumbBar.appendChild(lineBreak);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= 5; i++) {
+    // let placeholder = document.createElement('p');
+    // placeholder.innerHTML = 'Pic ' + i;
+    // thumbBar.appendChild(placeholder);
+
+    const newImage = document.createElement('img');
+    let srcAttribute = 'images/pic' + i + '.jpg';
+    newImage.setAttribute('src', srcAttribute)
+    thumbBar.appendChild(newImage);
+
+    newImage.onclick = function () {
+        let newSrcAttribute = newImage.getAttribute('src');
+        displayedImage.setAttribute('src', newSrcAttribute);
+    }
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function () {
+    let className = btn.getAttribute('class');
+    if (className == 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.innerHTML = 'lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    }
+    else {
+        btn.setAttribute('class', 'dark');
+        btn.innerHTML = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+}
